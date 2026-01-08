d493f5c85180f639559dbe6bffd07e85505115b3
Bilal Ahmad Butt <54913211+imbilalbutt@users.noreply.github.com>
2026-01-08 01:34:44 +0100

    Authentication using UI controller

---
+++ src/main/java/com/imbilalbutt/springauthdev/Security/SessionManagement.java
@@ -0,0 +1,36 @@
+package com.imbilalbutt.springauthdev.Security;
+
+import jakarta.servlet.SessionCookieConfig;
+import jakarta.servlet.SessionTrackingMode;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
+
+@Configuration
+@EnableWebMvc
+public class SessionManagement  implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/ui/");
+        registry.addViewController("/ui").setViewName("redirect:/ui/");
+    }
+
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+        return servletContext -> {
+            servletContext.setSessionTrackingModes(
+                    Collections.singleton(SessionTrackingMode.COOKIE)
+            );
+            SessionCookieConfig sessionCookieConfig =
+                    servletContext.getSessionCookieConfig();
+            sessionCookieConfig.setHttpOnly(true);
+            sessionCookieConfig.setSecure(false); // Set to true in production with HTTPS
+        };
+    }
+}
